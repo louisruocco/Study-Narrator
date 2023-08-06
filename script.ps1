bc18c0f3e1c638f89887b1d0a41092dd5ccabddc
Louis Ruocco <louisruocco1@gmail.com>
2023-08-06 17:30:24 +0100

    able to grab one random note, get it's content and exclude certain letters and symbols

--- script.ps1
+++ script.ps1
@@ -2,12 +2,20 @@
 $paths = "E:\Obsidian Vaults\IT\AZ-104"
 
 ##Get the child items and content of the docs in the paths
-function Read-Notes {
+function Get-Notes {
     $dirs = Get-ChildItem $paths -Exclude "images", "Exam Overview.md", "revision topics.txt"
     $getRandomFolder = $dirs | Sort-Object {Get-Random}
-    $getRandomFolder[0]
+    $firstFolder = $getRandomFolder[0]
+    
+    $notes = Get-ChildItem $firstFolder
+    $getRandomNote = $notes | Sort-Object {Get-Random}
+    $firstNote = $getRandomNote[0]
+    
+    $notePath = $firstNote.DirectoryName
+    $text = Get-Content $notePath\$firstNote
+    $text -replace '#', '' -replace "=", '' -replace '-', '' -replace 'Practice Questions.*', '' -replace 'Knowledge Check.*', '' -replace 'AZ104', '' -replace 'Exercises:.*', ''
 }
 
-Read-Notes
+Get-Notes
 
 # Read them out

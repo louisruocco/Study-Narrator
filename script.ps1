578631f1ee0684c9a4d3d377a21556c23569f12e
Louis Ruocco <louisruocco1@gmail.com>
2023-08-06 17:11:08 +0100

    changed code around and now able to randomise folders

--- script.ps1
+++ script.ps1
@@ -3,14 +3,9 @@ $paths = "E:\Obsidian Vaults\IT\AZ-104"
 
 ##Get the child items and content of the docs in the paths
 function Read-Notes {
-    $dirs = Get-ChildItem $paths
-    $test = foreach($dir in $dirs){
-       $notes = Get-ChildItem $paths\$dir -Exclude "images"
-       $text = Get-Content $notes
-       $text -replace '==', '' -replace '##', '' -replace "#", '' -replace '-', ''
-    }
-
-    $test
+    $dirs = Get-ChildItem $paths -Exclude "images", "Exam Overview.md", "revision topics.txt"
+    $getRandomFolder = $dirs | Sort-Object {Get-Random}
+    $getRandomFolder[0]
 }
 
 Read-Notes

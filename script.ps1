bdd33696072aab14025a728ca417057fd9ca8f1b
Louis Ruocco <louisruocco1@gmail.com>
2023-08-06 12:24:41 +0100

    able to retrieve list of child items from folders in paths variable

--- script.ps1
+++ script.ps1
@@ -4,7 +4,9 @@ $paths = "E:\Obsidian Vaults\IT\AZ-104"
 ##Get the child items and content of the docs in the paths
 function Read-Notes {
     $dirs = Get-ChildItem $paths
-    $dirs
+    foreach($dir in $dirs){
+       Get-ChildItem $paths\$dir
+    }
 }
 
 Read-Notes
